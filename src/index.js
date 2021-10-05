c62277cb8e9f9ebbe405f350cf37e5b4561104dc
Gustavo Sant'Anna <g_santanna@outlook.com>
2021-10-05 01:31:36 -0300

    Adicionado path (tentatia 2 deploy)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Route path="/react-portifoly" component={ App } />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
