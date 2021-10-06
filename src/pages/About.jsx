2b406527f4ff590f9e80133d935f20dbde400696
Gustavo Sant'Anna <g_santanna@outlook.com>
2021-10-06 03:13:22 -0300

    Diversas atualizações e criação da área "Home"

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,6 +7,12 @@ export default class About extends Component {
     return (
       <main>
         <h1>Sobre mim</h1>
+        <article className="about-text">
+          Nascido em Niterói-RJ no dia 18 de fevereiro de 1994, atualmente tenho 27 anos e moro em Nova Iguaçu-RJ.
+          Gosto de atividades ao ar livre, RPG de mesa, <s>dormir,</s> Rock, animes e socializar
+          Divido a minha vida com duas lindas meninas, uma delas é a minha esposa Kezia, a outra é minha filha Keila, de 1 ano.
+          Desde pequeno sou fascinado na área de tecnologia, computação e afins, a vida me distanciou desse caminho, mas agora, volto a navegar nessas águas, iniciando minha história de Desenvolvedor Web na Trybe.
+        </article>
       </main>
     );
   }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,56 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 // import { Link } from 'react-router-dom';
 // import PropTypes from 'prop-types';
+import {
+  GitHubIcon, FacebookIcon, LinkedInIcon,
+  ComputerIcon, KeyboardIcon, BoardPin
+} from '../images/svgFiles';
 
 export default class Home extends Component {
   render() {
     return (
-      <main>
-        <h1>Quem sou eu?</h1>
-        <article className="main-text">
-          Nascido em Niterói-RJ no dia 18 de fevereiro de 1994, atualmente tenho 27 anos e moro em Nova Iguaçu-RJ.
-          Gosto de atividades ao ar livre, RPG de mesa, <s>dormir,</s> Rock, animes e socializar
-          Divido a minha vida com duas lindas meninas, uma delas é a minha esposa Kezia, a outra é minha filha Keila, de 1 ano.
-          Desde pequeno sou fascinado na área de tecnologia, computação e afins, a vida me distanciou desse caminho, mas agora, volto a navegar nessas águas, iniciando minha história de Desenvolvedor Web na Trybe.
-        </article>
+      <main className="home">
+        <h2>Seja Bem Vindo!</h2>
+        <div className="home-div">
+          <div className="computer-div">
+            { ComputerIcon }
+            <div className="computer-photo" />
+            <div className="sociallinks-div">
+              <a
+                href="https://github.com/Warywise"
+                rel="noreferrer"
+                target="_blank"
+              >{ GitHubIcon }</a>
+              <a
+                href="https://web.facebook.com/gustavo.santanna.77"
+                rel="noreferrer"
+                target="_blank"
+              >{ FacebookIcon }</a>
+              <a
+                href="https://www.linkedin.com/in/g-s-s/"
+                rel="noreferrer"
+                target="_blank"
+              >{ LinkedInIcon }</a>
+            </div>
+          </div>
+          { KeyboardIcon }
+        </div>
+        <aside className="aside-board">
+          <div className="board-fixer"/>
+          <div>
+            { BoardPin }
+            <Link onClick={ this.handleClick } to="/react-portifoly/skills">Skills</Link>
+          </div>
+          <div>
+            { BoardPin }
+            <Link onClick={ this.handleClick } to="/projects">Projetos</Link>
+          </div>
+          <div>
+            { BoardPin }
+            <Link onClick={ this.handleClick } to="/about">Sobre</Link>
+          </div>
+        </aside>
       </main>
     );
   }
