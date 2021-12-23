815952ae45c0032d5f44704a57b6895e264fef30
Gustavo Sant'Anna <g_santanna@outlook.com>
2021-12-23 19:51:37 -0300

    SASS CSS adicionado e Header.css alterado para Header.scss

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/Header.css';
+import '../styles/Header.scss';
 
 export default class Header extends Component {
   handleClick = ({ target }) => {
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
 // import PropTypes from 'prop-types';
 
+import { ComputerIcon } from '../images/svgFiles';
+
 export default class Projects extends Component {
   render() {
     return (
       <main>
         <h1>Meus projetos</h1>
+        <div className='computer-div'>{ ComputerIcon }</div>
       </main>
     );
   }
